b7729085f68de46e74ce027954e3a2201522e0ce
Sadiq <sadiqalsandouk@hotmail.com>
2023-05-10 17:42:48 +0100

    working with no styles

---
+++ src/TodoForm.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+export function ToDoForm({onSubmit}) {
+    const [newItem, setNewItem] = useState("")
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        if (newItem === "") return 
+        onSubmit(newItem)
+        setNewItem("")
+    
+      }
+
+    return (
+    <form onSubmit={handleSubmit} className="new-item-form" >
+    <div className="form-row">
+    <label htmlFor="item">Enter a Task:</label>
+    <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"></input>
+    </div>
+    <button className="btn">Add</button>
+  </form>
+    )
+}
